79488e3c16edc8dd624ab41f8aa55c9547fb1a95
fatmah <fatmahedfi@gmail.com>
2014-04-17 18:16:16 +0100

    fix bug

--- src/Projet/FacebookBundle/ProjetFacebookBundle.php
+++ src/Projet/FacebookBundle/ProjetFacebookBundle.php
@@ -3,28 +3,28 @@
 namespace Projet\FacebookBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\Container;
+//use Symfony\Component\DependencyInjection\Container;
 
 class ProjetFacebookBundle extends Bundle
 {
-	protected $container;
+// 	protected $container;
 	
-	public function __construct(Container $container) {
-		$this->container = $container;
-	}
+// 	public function __construct($container) {
+// 		$this->container = $container;
+// 	}
 	
-	public function onKernelRequest(GetResponseEvent $event)
-	{
-		$request = $event->getRequest();
-		$mobileDetector = $this->container->get('mobile_detect.mobile_detector');
-		$mobile = $mobileDetector->isMobile();
-		if(!$mobile) {
-			$this->container->setParameter('session.storage.options.use_cookies', 0);
-			$this->container->setParameter('session.storage.options.url_rewriter.tags', 'a=href,area=href,frame=src,input=src,form=action');
-			$this->container->setParameter('session.storage.options.use_only_cookies', 0);
-			$this->container->setParameter('session.storage.options.use_trans_sid', 1);
-		}
+// 	public function onKernelRequest(GetResponseEvent $event)
+// 	{
+// 		$request = $event->getRequest();
+// 		$mobileDetector = $this->container->get('mobile_detect.mobile_detector');
+// 		$mobile = $mobileDetector->isMobile();
+// 		if(!$mobile) {
+// 			$this->container->setParameter('session.storage.options.use_cookies', 0);
+// 			$this->container->setParameter('session.storage.options.url_rewriter.tags', 'a=href,area=href,frame=src,input=src,form=action');
+// 			$this->container->setParameter('session.storage.options.use_only_cookies', 0);
+// 			$this->container->setParameter('session.storage.options.use_trans_sid', 1);
+// 		}
 		
-	}
+// 	}
 	
 }
